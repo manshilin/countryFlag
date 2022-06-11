a560ef5a24ffb401fb208297ca74d9c70b95cedc
Sergii Manshylin <sergeymanshilin@gmail.com>
2022-06-11 13:21:05 +0300

    Поключил мелодию и блок очков

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
-console.log("HI");
-console.log("My first commit");
-console.log("Я с вами))");
+// Событие "нажатие на иконку звука" 
+let audioPlayer = document.querySelector("audio");
+let sound = "off"; // "on"
+let soundButton = document.querySelector("#sound img");
+soundButton.onclick = function() {
+    
+    if(sound == "off") {
+        soundButton.src = "images/sound_on black.png";
+        sound = "on";
+        audioPlayer.play();       
+    }
+    else{
+        soundButton.src = "images/mute_sound_black.gif";
+        sound = "off";
+        audioPlayer.pause();
+    }    
+}
